a2cfca7dfb39639fc91048c6512a72d040cfbc8d
Isa <isabella.c.aigner@gmail.com>
2025-04-13 14:52:52 +0200

    fix filter layout

--- src/components/FilterGroup.vue
+++ src/components/FilterGroup.vue
@@ -30,22 +30,13 @@ const toggleFilter = (filter: string) => {
 <template>
   <div class="filter-group">
     <TransitionGroup name="tag">
-      <div class="flex gap-2">
-        <template v-for="(filter, index) in filters.slice(0, 4)" :key="filter.code">
+      <div class="flex gap-2 filter-group-el">
+        <template
+          v-for="(filter, index) in filters"
+          :key="filter.code">
           <FilterButton
             :filter="filter"
             :is-selected="selectedFilter === filter.code"
-            :class="{ 'last-of-line': index === 3 }"
-            @toggle-filter="toggleFilter(filter.code)"
-          />
-        </template>
-      </div>
-      <div class="flex gap-2">
-        <template v-for="(filter, index) in filters.slice(4, 8)" :key="filter.code">
-          <FilterButton
-            :filter="filter"
-            :is-selected="selectedFilter === filter.code"
-            :class="{ 'last-of-line': index === 3 }"
             @toggle-filter="toggleFilter(filter.code)"
           />
         </template>
