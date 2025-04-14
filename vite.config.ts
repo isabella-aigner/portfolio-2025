9e816eee40201f8422a8851509c1f83774050084
Isa <isabella.c.aigner@gmail.com>
2025-04-14 18:00:31 +0200

    use a different package for image reduction

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import viteImagemin from 'vite-plugin-imagemin'
+import imagePresets, { widthPreset } from 'vite-plugin-image-presets';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    viteImagemin({
-      webp: {
-        quality: 80
-      }
+    imagePresets({
+      default: widthPreset({
+        class: 'responsive-image',
+        widths: [320, 640, 960],
+        formats: ['webp', 'jpeg'],
+      }),
     }),
     tailwindcss(),
   ],
